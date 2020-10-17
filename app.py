0eafb2171aea4c17468ebf6b6125db22f2d373f5
Inkyu Choi <aika823@naver.com>
2020-10-17 10:36:34 +0000

    added Beutifulsoup for Crawling

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import requests
+import urllib.request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
@@ -12,6 +18,13 @@ def hello_world():
 def home():
     return render_template('index.html')
 
+
+@app.route('/crawl')
+def crawl():
+    html = urllib.request.urlopen("https://wevity.com")
+    soup = BeautifulSoup(html, "html.parser")
+    main_section = soup.find_all('div', 'main-section')
+    return str(main_section)
 
 if __name__ == '__main__':
     app.run(debug=True)
